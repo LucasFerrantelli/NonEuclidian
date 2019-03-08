4a9b006ce635c465d9e78aa2e76fd7b9c1ca628c
zageen <zageen60@gmail.com>
2019-03-08 18:29:13 +0100

    Hub

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class DoorScript : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Vector3.Distance(transform.position, Camera.main.transform.position)< 3 && Input.GetKeyDown("e"))
+        {
+            anim.Play("DoorAnim");
+        }
+    }
+
+}
